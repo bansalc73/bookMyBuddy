07c347868c55637fd3e2f0ce3c8a715489251846
Chirag <bansalc73@gmail.com>
2023-05-16 01:13:54 +0530

    Hosting 3.0

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 mongoose.connection.on("disconnected", () =>{console.log("Disconnected")})
 
